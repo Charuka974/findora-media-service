6818eaeefa2bbb3c362725216d7743bff7d31c8e
Charuka974 <charuka974@gmail.com>
2026-08-23 17:45:01 +0530

    feat: implement media service with local and Google Cloud Storage upload and deletion support

--- src/main/java/com/findora/services/media_service/service/MediaService.java
+++ src/main/java/com/findora/services/media_service/service/MediaService.java
@@ -3,9 +3,13 @@ package com.findora.services.media_service.service;
 import com.findora.services.media_service.dto.MediaUploadResponse;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface MediaService {
 
     MediaUploadResponse uploadFile(MultipartFile file);
+
+    List<MediaUploadResponse> uploadMultipleFiles(List<MultipartFile> files);
 
     void deleteFile(String objectName);
 }
